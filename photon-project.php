bfb67b890fdda8329603b355845cf10e9294c7a9
i-am-phi <phi@i-am-phi.net>
2020-09-01 17:48:25 -0700

    add css to pipeline

--- photon-project.php
+++ photon-project.php
@@ -81,7 +81,7 @@ class PhotonProjectPlugin extends Plugin
 
       // styles
       $css = 'plugin://photon-project/assets/project.css';
-      $assets->addCss($css, 100, false, 'photon-plugins' );
+      $assets->addCss($css, 100, 'pipeline', 'photon-plugins' );
 
       // only load the vars if this datatype page
       if ($page->template() == 'project')
